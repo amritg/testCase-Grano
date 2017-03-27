a265dde13abee5654ad6d4ceb334ff92bc375006
Amrit Gautam <gautamamrit24@gmail.com>
2017-03-27 12:32:18 +0300

    Configure Port on which application will be on Heroku'

--- server.js
+++ server.js
@@ -45,7 +45,9 @@ app.post('/api/companyinfo', function(req, res){
 });
 
 //  Server Configuration
-
-app.listen(3000, function(){
-    console.log("App listening on port: 3000");
+    // set the port of our application
+    // process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+    console.log("App listening on port:" + port);
 });
